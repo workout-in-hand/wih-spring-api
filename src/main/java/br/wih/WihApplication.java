5dd83bd8e77106c3a993c08642f1219d89275270
carlosBSMedeiros <carlos.bs.medeiros@gmail.com>
2022-05-13 17:24:00 -0300

    inicio do projeto

---
+++ src/main/java/br/wih/WihApplication.java
@@ -0,0 +1,13 @@
+package br.wih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WihApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/wih/WihApplicationTests.java
@@ -0,0 +1,13 @@
+package br.wih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WihApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
